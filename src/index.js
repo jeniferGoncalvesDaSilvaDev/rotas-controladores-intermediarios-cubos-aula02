35ba6e9b0e6f0af4a7deb72604c1ebb54365a89f
Jonathan Goncalves da Silva <6199453@univali.br>
2023-09-20 21:49:37 -0300

    dia vencido

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 const express = require('express')
 
 const app = express()
-
-app.get('/', (req, res) => {
-    //informaçoes da requisiçao
-  //console.log(req)
-  res.send('Páginal inicial')
+const professores= [{
+  id:1,
+  nome:'Guido',
+  stack:'back-end'
+},
+{
+  id:2,
+  nome:'Maria',
+  stack:'front-end'
+},
+{
+  id:3,
+  nome:'Pedro',
+  stack:'front-end'
+},
+{
+  id:4,
+  nome:'Ana',
+  stack:'back-end'
+}
+]
+app.get('/professores', function rota(req, res) {
+  res.send(professores)
 })
 
-app.listen(3000, () => {
+app.listen(3000, function mensagemNoTerminal () {
   console.log('Example app listening on port 3000!')
 })
